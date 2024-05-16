855d7445945201fedd262951b96892feb97845a4
Farhan Abbas <fabbas.ambition@gmail.com>
2024-05-16 12:09:02 +0530

    updated

--- api/foodItemsAndRecipesFinder.py
+++ api/foodItemsAndRecipesFinder.py
@@ -74,7 +74,7 @@ def foodItemsAndRecipesFinder():
 
     return {'data': responseReturn}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
